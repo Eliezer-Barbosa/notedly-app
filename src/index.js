f14b7db49051a08b81ce692d59a4c930031193b8
Eliezer <eliezer1789j@gmail.com>
2022-01-20 13:40:36 +0000

    installing graphql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+// run server on PORT from .env file or 4000
+const port = process.env.PORT || 4000
+
+// schema
+const typeDefs = gql`
+type Query {
+    hello: String
+}`
+
+// resolver
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World'
+    }
+}
 
 const app = express()
-const port = process.env.PORT || 4000
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers})
+
+// Apply Apollo GraphQL middleware and set path to /api
+server.applyMiddleware({ app, path: '/api' })
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+app.listen({ port }, () => console.log(`GraphQL server running at http://localhost:${port}${server.graphqlPath}`))
