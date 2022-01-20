b32fab9c7e6b18ae674054b27cc631f22990403e
Eliezer <eliezer1789j@gmail.com>
2022-01-20 15:28:18 +0000

    reading and writing data from DB

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ const { ApolloServer, gql } = require("apollo-server-express");
 require("dotenv").config();
 
 const db = require("./db");
+const models = require("./models");
 
 // run server on PORT from .env file or 4000
 const port = process.env.PORT || 4000;
-
 const DB_HOST = process.env.DB_HOST;
-
-let notes = [
-  { id: "1", content: "This is a note", author: "Adam Scott" },
-  { id: "2", content: "This is another note", author: "Harlow Everly" },
-  { id: "3", content: "Oh hey look, another note!", author: "Riley Harrison" },
-];
 
 // schema
 const typeDefs = gql`
@@ -38,20 +32,19 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => "Hello World",
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     },
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let notevalue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: "Adam Scott",
-      };
-      notes.push(notevalue);
-      return notevalue;
+      });
     },
   },
 };
