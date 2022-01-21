b60a77a7857542a433e5d691aad9c88556f4103e
Eliezer <eliezer1789j@gmail.com>
2022-01-21 14:14:17 +0000

    refactor - separate schemas and resolvers for flexibility

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
-const typeDefs = require("./schema");
+const { ApolloServer } = require("apollo-server-express");
 require("dotenv").config();
 
+// Local module imports
 const db = require("./db");
 const models = require("./models");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
 // run server on PORT from .env file or 4000
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-// resolver
-const resolvers = {
-  Query: {
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    },
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: "Adam Scott",
-      });
-    },
-  },
-};
 
 const app = express();
 
@@ -36,7 +18,13 @@ const app = express();
 db.connect(DB_HOST);
 
 // Apollo Server setup
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  },
+});
 
 // Apply Apollo GraphQL middleware and set path to /api
 server.applyMiddleware({ app, path: "/api" });
