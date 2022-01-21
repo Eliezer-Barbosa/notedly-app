3bc270d3bd77a8e61de84e6b36f887021ed4dd47
Eliezer <eliezer1789j@gmail.com>
2022-01-21 16:44:39 +0000

    adding the user to the resolver context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // Local module imports
@@ -17,12 +18,32 @@ const app = express();
 // connect to the DB
 db.connect(DB_HOST);
 
+// get the user info from a JWT
+const getUser = (token) => {
+  if (token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if there's a problem with the token, throw an error
+      throw new Error("Session invalid");
+    }
+  }
+};
+
 // Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // for now, log the user to the console
+    console.log(user);
+    // add the db models and the user to the context
+    return { models, user };
   },
 });
 
